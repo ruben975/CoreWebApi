2285dc3d1d8e943eaf54ebcb266fc8e916d5b068
Nagy Ruben <nagy.ruben97@gmail.com>
2021-05-06 17:15:43 +0300

    Add project files.

---
+++ WebApplication1/Migrations/20210506141334_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+
+
+
+    }
+}
